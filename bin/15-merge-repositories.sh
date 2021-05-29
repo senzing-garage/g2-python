35de025e1185d1074e15c7bd18092f4f98846b03
Michael Dockter <michael@dockter.com>
2021-05-29 15:57:04 -0400

    #2 Savepoint

--- bin/15-merge-repositories.sh
+++ bin/15-merge-repositories.sh
@@ -11,17 +11,17 @@ for SUBMODULE in ${SUBMODULES[@]};
 do
 
     # Get metadata.
-    
+
     IFS=";" read -r -a SUBMODULE_DATA <<< "${SUBMODULE}"
     SUBMODULE_NAME="${SUBMODULE_DATA[0]}"
     SUBMODULE_VERSION="${SUBMODULE_DATA[1]}"
     SUBMODULE_ARTIFACT="${SUBMODULE_DATA[2]}"
-    
+
     echo "Processing ${SUBMODULE_NAME}:${SUBMODULE_VERSION}"
-    
+
     # Get requested version of submodule.
-    
-    cd ${GIT_REPOSITORY_DIR}/${SUBMODULE_NAME}
+
+    cd ${GIT_ACCOUNT_DIR}/${SUBMODULE_NAME}
     git checkout main
     git pull
     git checkout ${GIT_SUBMODULE_BRANCH}
---
+++ bin/43-pull-repositories.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# Read metadata.
+
+source 01-user-variables.sh
+source 11-submodules.sh
+
+# Process each entry.
+
+for SUBMODULE in ${SUBMODULES[@]};
+do
+
+    # Get metadata.
+
+    IFS=";" read -r -a SUBMODULE_DATA <<< "${SUBMODULE}"
+    SUBMODULE_NAME="${SUBMODULE_DATA[0]}"
+    SUBMODULE_VERSION="${SUBMODULE_DATA[1]}"
+    SUBMODULE_ARTIFACT="${SUBMODULE_DATA[2]}"
+
+    echo "Processing ${SUBMODULE_NAME}:${SUBMODULE_VERSION}"
+
+    # Get requested version of submodule.
+
+    cd ${GIT_ACCOUNT_DIR}/${SUBMODULE_NAME}
+    git pull
+
+done

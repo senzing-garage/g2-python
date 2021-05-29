fdc6d4f789c951d20b219a58dc9a97957ed3b1b2
Michael Dockter <michael@dockter.com>
2021-05-29 10:23:50 -0400

    #2 Savepoint

--- bin/03-make-copy-files-from-senzing-install.sh
+++ bin/03-make-copy-files-from-senzing-install.sh
@@ -6,4 +6,4 @@ source 01-user-variables.sh
 
 # Create 03-copy-files-from-senzing-install.sh
 
-${GITHUB_UTIL_DIR}/github-util.py print-copy-files-from-senzing-install > ${BIN_DIR}/03-copy-files-from-senzing-install.sh
+${GITHUB_UTIL_DIR}/github-util.py print-copy-files-from-senzing-install > ${BIN_DIR}/04-copy-files-from-senzing-install.sh

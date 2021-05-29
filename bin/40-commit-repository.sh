63566d1368717ff6c0030e73f2afcd6110533451
Michael Dockter <michael@dockter.com>
2021-05-29 10:08:55 -0400

    #2 Savepoint

--- bin/40-commit-repository.sh
+++ bin/40-commit-repository.sh
@@ -7,6 +7,10 @@ source 01-user-variables.sh
 # Tag the current version of the collection.
 
 cd ${GIT_REPOSITORY_DIR}
+git checkout ${GIT_REPOSITORY_BRANCH}
 git add ${OUTPUT_DIR}/*
 git commit -a -m "#2 SenzingAPI ${SENZING_VERSION}"
 git push
+git checkout main
+git pull
+git checkout ${GIT_REPOSITORY_BRANCH}
